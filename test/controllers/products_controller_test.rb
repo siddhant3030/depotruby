ab7d78d84c491d1e3b6c42d48ec95c134a9fe583
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-11 12:17:33 +0530

    written some test case for the controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title:'siddhant singh',
+      description: 'having fun',
+      image_url: 'lorem.jpg'
+      price: 20 
+    }
   end
 
   test "should get index" do
@@ -17,7 +23,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+     post :create, product: @update
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +40,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_url(@product)
   end
 
