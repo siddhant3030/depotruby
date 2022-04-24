0f6865d0a3c21aa1389d24dc6397e59b8e4365c6
siddhant3030 <ssiddhant3030@gmail.com>
2022-04-24 16:36:02 +0530

    graph

--- graph.py
+++ graph.py
@@ -1,2 +1,29 @@
+from operator import truediv
+
+
 class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
     
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list = []
+            return True
+        return False
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+            return True
+        return False
